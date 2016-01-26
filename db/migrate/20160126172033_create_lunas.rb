487b041f6d9300934c114d2311b827c27595e96a
medwards1771 <meredith.edwards1771@gmail.com>
2016-01-26 12:24:31 -0500

    add columns to luna table

--- db/migrate/20160126172033_create_lunas.rb
+++ db/migrate/20160126172033_create_lunas.rb
@@ -1,7 +1,11 @@
 class CreateLunas < ActiveRecord::Migration
   def change
     create_table :lunas do |t|
-
+      t.integer :age
+      t.integer :illumination
+      t.string :stage
+      t.integer :DFCOE
+      t.integer :DFS
       t.timestamps null: false
     end
   end
