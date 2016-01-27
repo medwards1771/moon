50b9852c9091677a9fa1e6442df8208b89d41b49
medwards1771 <meredith.edwards1771@gmail.com>
2016-01-27 09:01:51 -0500

    adds day 10-29 moon images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,33 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( day_1.jpg )
+Rails.application.config.assets.precompile += %w( day_2.jpg )
+Rails.application.config.assets.precompile += %w( day_3.jpg )
+Rails.application.config.assets.precompile += %w( day_4.jpg )
+Rails.application.config.assets.precompile += %w( day_5.jpg )
+Rails.application.config.assets.precompile += %w( day_6.jpg )
+Rails.application.config.assets.precompile += %w( day_7.jpg )
+Rails.application.config.assets.precompile += %w( day_8.jpg )
+Rails.application.config.assets.precompile += %w( day_9.jpg )
+Rails.application.config.assets.precompile += %w( day_10.jpg )
+Rails.application.config.assets.precompile += %w( day_11.jpg )
+Rails.application.config.assets.precompile += %w( day_12.jpg )
+Rails.application.config.assets.precompile += %w( day_13.jpg )
+Rails.application.config.assets.precompile += %w( day_14.jpg )
+Rails.application.config.assets.precompile += %w( day_15.jpg )
+Rails.application.config.assets.precompile += %w( day_16.jpg )
+Rails.application.config.assets.precompile += %w( day_17.jpg )
+Rails.application.config.assets.precompile += %w( day_18.jpg )
+Rails.application.config.assets.precompile += %w( day_19.jpg )
+Rails.application.config.assets.precompile += %w( day_20.jpg )
+Rails.application.config.assets.precompile += %w( day_21.jpg )
+Rails.application.config.assets.precompile += %w( day_22.jpg )
+Rails.application.config.assets.precompile += %w( day_23.jpg )
+Rails.application.config.assets.precompile += %w( day_24.jpg )
+Rails.application.config.assets.precompile += %w( day_25.jpg )
+Rails.application.config.assets.precompile += %w( day_26.jpg )
+Rails.application.config.assets.precompile += %w( day_27.jpg )
+Rails.application.config.assets.precompile += %w( day_28.jpg )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
